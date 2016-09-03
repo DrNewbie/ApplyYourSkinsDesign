03000e2d8a8816ed678584953f27c9d00199c81f
Dr_Newbie <tf2baidonation@gmail.com>
2016-09-04 01:16:44 +0800

    6.1 ; not a big deal

--- Apply Your Skins Design/lua/akimboweaponbase.lua
+++ Apply Your Skins Design/lua/akimboweaponbase.lua
@@ -1,5 +1,5 @@
 local _f_AkimboWeaponBase_1_create_second_gun = AkimboWeaponBase._create_second_gun
-function AkimboWeaponBase:_create_second_gun()
-	_f_AkimboWeaponBase_1_create_second_gun(self)
+function AkimboWeaponBase:_create_second_gun(...)
+	_f_AkimboWeaponBase_1_create_second_gun(self, ...)
 	SkinEditor_Patch:Apply_Skins(self._second_gun)
 end
